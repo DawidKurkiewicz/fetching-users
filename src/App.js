a92041cb6277f360008cbb326f6070f742d4df2a
DawidKurkiewicz <dawidkurkiewicz516@gmail.com>
2018-12-22 02:02:46 +0100

    deleted unessesary files

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
 import MenuItem from 'material-ui/MenuItem'
 import Navbar from './Menu'
-import Statement from './Statement'
 import Users from './Users'
 import About from './About'
 import BadUrl from './BadUrl'
@@ -10,42 +9,10 @@ import BadUrl from './BadUrl'
 const style = {
   links: {
     textDecoration: 'none'
-  },
-  button: {
-    position: 'fixed',
-    right: 20,
-    bottom: 10
   }
 }
 
-class App extends React.Component {
-  state = {
-    isStatementOpen: false,
-    statementMessage: ''
-  }
-
-  toggleStatement = (message) => this.setState({
-    isStatementOpen: !this.state.isStatementOpen,
-    statementMessage: message || ''
-  })
-
-  render() {
-    return (
-      <div>
-        <RouterTK
-          toggleStatement={this.toggleStatement}
-        />
-        <Statement
-          isStatementOpen={this.state.isStatementOpen}
-          handleRequestClose={this.toggleStatement}
-          statementMessage={this.state.statementMessage}
-        />
-      </div>
-    )
-  }
-}
-
-class RouterTK extends React.PureComponent {
+class App extends React.PureComponent {
   render() {
     return (
       <Router>
@@ -68,13 +35,13 @@ class RouterTK extends React.PureComponent {
             </Link>
           </Navbar>
           <div>
-          <Switch>
+            <Switch>
 
-            <Route path="/" exact={true} component={Users}></Route>
-            <Route path='/users' component={Users}></Route>
-            <Route path='/about' component={About}></Route>
-            <Route component={BadUrl} />
-</Switch>
+              <Route path="/" exact={true} component={Users}></Route>
+              <Route path='/users' component={Users}></Route>
+              <Route path='/about' component={About}></Route>
+              <Route component={BadUrl} />
+            </Switch>
           </div>
         </div>
       </Router>
