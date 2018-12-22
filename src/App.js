77beaf30fa97a45216aa24c64d3063d2397ab9f8
DawidKurkiewicz <dawidkurkiewicz516@gmail.com>
2018-12-22 01:57:38 +0100

    added material-ui

--- src/App.js
+++ src/App.js
@@ -1,29 +1,85 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
+import MenuItem from 'material-ui/MenuItem'
+import Navbar from './Menu'
+import Statement from './Statement'
 import Users from './Users'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
-import Menu from './Menu'
 import About from './About'
 import BadUrl from './BadUrl'
 
-class App extends React.Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <div>
-          <Route component={Menu} />
-          <Switch>
-
-          <Route path='/' exact component={Users} />
-          <Route path='/' exact component={Users} />
-          <Route path='/users' exact component={Users} />
-          <Route path='/about' component={About} />
-          <Route component={BadUrl} />
-          </Switch>
-
-
-        </div>
-      </BrowserRouter>
-    );
+const style = {
+  links: {
+    textDecoration: 'none'
+  },
+  button: {
+    position: 'fixed',
+    right: 20,
+    bottom: 10
   }
 }
+
+class App extends React.Component {
+  state = {
+    isStatementOpen: false,
+    statementMessage: ''
+  }
+
+  toggleStatement = (message) => this.setState({
+    isStatementOpen: !this.state.isStatementOpen,
+    statementMessage: message || ''
+  })
+
+  render() {
+    return (
+      <div>
+        <RouterTK
+          toggleStatement={this.toggleStatement}
+        />
+        <Statement
+          isStatementOpen={this.state.isStatementOpen}
+          handleRequestClose={this.toggleStatement}
+          statementMessage={this.state.statementMessage}
+        />
+      </div>
+    )
+  }
+}
+
+class RouterTK extends React.PureComponent {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Navbar>
+            <Link
+              to='/users'
+              style={style.links}
+            >
+              <MenuItem>
+                Users
+          </MenuItem>
+            </Link>
+            <Link
+              to='/about'
+              style={style.links}>
+              <MenuItem>
+                About
+          </MenuItem>
+            </Link>
+          </Navbar>
+          <div>
+          <Switch>
+
+            <Route path="/" exact={true} component={Users}></Route>
+            <Route path='/users' component={Users}></Route>
+            <Route path='/about' component={About}></Route>
+            <Route component={BadUrl} />
+</Switch>
+          </div>
+        </div>
+      </Router>
+    )
+  }
+}
+
 export default App;
--- src/Menu.js
+++ src/Menu.js
@@ -1,38 +1,51 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
-import './Menu.css'
-
-
-class Menu extends React.Component {
-
+import AppBar from 'material-ui/AppBar';
+import Drawer from 'material-ui/Drawer'
+class Navbar extends React.Component {
+    state = {
+        isDrawerOpen: false
+    }
+    toggleDrawer = () => this.setState({ isDrawerOpen: !this.state.isDrawerOpen })
     render() {
         return (
-            <div className="title-container">
-                <div className="title"> Recrutation Task</div>
-                <div >
-                    <div className="link-container">
-                        <Link
-                            className="link"
-                            to='/users'
-                        >
+            <div>
+                <AppBar
+                    title={"Recrutation Task"}
+                    onLeftIconButtonClick={this.toggleDrawer}
 
-                            <div>Users</div>
-                        </Link>
-                    </div>
-                    <div className="link-container">
-                        <Link
-                            className="link"
-                            to='/about'
-                        >
-                            <div>About</div>
-                        </Link>
-                    </div>
+                />
+                <Drawer
+                    docked={false}
+                    width={200}
+                    open={this.state.isDrawerOpen}
+                    onRequestChange={this.toggleDrawer}
+                >
+                    {
+                        this.props.children ?
+                            this.props.children.map ?
+                                this.props.children.map(child => (
+                                    React.cloneElement(
+                                        child,
+                                        {
+                                            onClick: this.toggleDrawer,
+                                            key: child.props.to
+                                        }
+                                    )
+                                ))
+                                :
+                                React.cloneElement(
+                                    this.props.children,
+                                    { onClick: this.toggleDrawer }
+                                )
+                            :
+                            null
 
-                </div>
+                    }
+                </Drawer>
             </div>
         )
     }
 
 }
 
-export default Menu
+export default Navbar
